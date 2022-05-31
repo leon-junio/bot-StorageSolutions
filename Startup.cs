ee4d9b01f77ae558f4e742a1447ed39fe636a632
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-05-30 21:26:52 -0300

    Startup configurado com sucesso

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+//@author Leon Jr
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
